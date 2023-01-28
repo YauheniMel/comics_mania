33ab7151d7988318aa6b39a4f6c67a38653680e2
YauheniMel <207melnik@gmail.com>
2023-01-28 17:25:21 +0300

    feat: Defining global colors

---
+++ src/styles/colors/global.ts
@@ -0,0 +1,10 @@
+export const GLOBAL_COLORS = {
+  red: '#E54752',
+  blue: '#4A47E5',
+  green: '#47E574',
+  orange: '#E58947',
+  yellow: '#E5CB47',
+  black: '#21201F',
+  gray: '#A19F9D',
+  white: '#F2F1F0',
+};
