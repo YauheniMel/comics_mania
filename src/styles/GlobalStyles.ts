2946b660c42274190727dc51f42bfa400e5d70be
YauheniMel <207melnik@gmail.com>
2023-01-28 17:49:19 +0300

    ref: Override global styles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  *,
+  *:before,
+  *:after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  html, body {
+    font-size: 20px;
+  }
+
+  body * {
+    font-size: 20px;
+  }
+`;
